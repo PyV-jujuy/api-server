9a2b9ffaf2895dc47cad22c779ae5cb2c52393e1
pyv-jujuy <pyvjujuy@gmail.com>
2023-06-27 16:50:36 -0300

    refactor(padron): use Url Class on route

--- src/modules/Padron/drives/routes/index.ts
+++ src/modules/Padron/drives/routes/index.ts
@@ -2,13 +2,15 @@ import { Router } from 'express'
 
 import PadronRepository from '../repositories/supabase'
 import PadronController from './controller'
+import Url from '../../../shared/types/Url'
 
 const PATH = '/padron'
+const url = new Url(PATH)
 
 const PadronRouter = Router()
 const repository = new PadronRepository()
 const controller = new PadronController(repository)
 
-PadronRouter.get(PATH, controller.routeHandler)
+PadronRouter.get(url.PATH, controller.routeHandler)
 
 export default PadronRouter
