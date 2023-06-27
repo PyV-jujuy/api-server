94f40e28612a538d031aa98b0db65debc86ca915
pyv-jujuy <pyvjujuy@gmail.com>
2023-06-27 15:01:53 -0300

    fix(Padron): update search repository

--- src/modules/Padron/services/SearchPerson/SearchPersonRepository.ts
+++ src/modules/Padron/services/SearchPerson/SearchPersonRepository.ts
@@ -1,10 +1,10 @@
 import type Paginated from '../../../shared/types/Paginated'
 import type PersonRepositoryDTO from '../../core/dto/PersonRepositoryDto'
 
-type ResultByMatricule = Promise<Paginated<PersonRepositoryDTO>>
+type ResultSearch = Promise<Paginated<PersonRepositoryDTO>>
 export default abstract class Repository {
   abstract search(
     person: Partial<PersonRepositoryDTO>,
     page: number
-  ): ResultByMatricule
+  ): ResultSearch
 }
